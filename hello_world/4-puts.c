297c237b9447f37671d9c0208d4e275f6954730a
Monica-Holberton <monicafarid7@gmail.com>
2025-03-02 20:45:37 +0000

    Put!

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-int main () {
-	puts ("Programming is like building a multilingual puzzle");
-	return 0;
+
+/*
+ * This is the main function
+ * it returns 0
+ * it prints a string*/
+
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
