349226055e8916aeea0f658c9c398b4a3169e509
Monica <Monicafarid7@gmail.com>
2025-03-02 22:57:15 +0200

    Update 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,9 +3,10 @@
 /*
  * This is the main function
  * it returns 0
- * it prints a string*/
+ * it prints a string
+ */
 
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
