a894138104caa4cd4b82f7da1a315b838e9fea73
Monica-Holberton <monicafarid7@gmail.com>
2025-03-05 19:42:00 +0000

    Putchar!

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    char str[] = "_putchar";
+    int i;
+
+     while (str[i] != '\0')
+    {
+        putchar(str[i]);
+    }
+    putchar('\n');
+
+    return (0);
+}
