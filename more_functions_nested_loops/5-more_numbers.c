8cac2e2aaa3ac1cc12b32b4cf86b90e452f1e87a
Monica-Holberton <monicafarid7@gmail.com>
2025-03-08 11:12:43 +0000

    More Numbers!

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints numbers from 0 to 14, ten times
+ */
+void more_numbers(void)
+{
+    int i, num;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (num = 0; num <= 14; num++)
+        {
+            if (num > 9)  /* Print first digit for numbers >= 10 */
+                _putchar((num / 10) + '0');
+
+            _putchar((num % 10) + '0'); /* Print second digit */
+        }
+        _putchar('\n');
+    }
+}
