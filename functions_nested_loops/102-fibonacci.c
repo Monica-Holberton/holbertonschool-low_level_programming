cac7941e58877ba970ac447f74c12691103b34f3
Monica-Holberton <monicafarid7@gmail.com>
2025-03-09 16:11:19 +0000

    Fibonacci!

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -1,20 +1,35 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the first 50 Fibonacci numbers, starting with 1 and 2.
+ *
+ * Description: print the first 50 numbers in the Fibonacci sequence.
+ * number is the sum of the two (old and previous).
+ *
+ * The numbers must be separated by comma, followed by a new line at the end.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
+{
+int count = 0;
+long old = 0;
+long prev = 1;
 
-    int count =0;
-    long old=0;
-    long prev=1;
 
-    while (count<50);
-    printf("%lu", old+ prev);
-old=prev;
+while
+(count < 50);
+
+printf("%lu", old + prev);
+old = prev;
 prev = current;
-    putchar(',');
-    count++;
+putchar(' , ');
+count++;
+
 }
-    ptrintf ("The size of long %ld",sizeof(x));
-    putchar('\n');
-    return (0);
+ptrintf("The size of long %ld", sizeof(x));
+putchar('\n');
+return (0);
 }
+
