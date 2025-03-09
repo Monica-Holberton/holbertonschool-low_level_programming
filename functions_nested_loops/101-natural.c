5c484e2b215a02aec4614bb5bd2914edce0fe649
Monica-Holberton <monicafarid7@gmail.com>
2025-03-09 15:32:38 +0000

    Natural!

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
+/**
+ * main - Computes and prints the sum of all multiples of 3 or 5 below 1024
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int sum=0;
-    for(int c=0; c<1024; c++){
-        if (c%3==0 || c%5==0){
-            sum= c + sum
+int sum = 0;
+int c;
 
-        }
-    }
-    printf("The Sum %d \n",sum);
-    return (0);
+for (c = 0; c < 1024; c++)
+{
+if (c % 3 == 0 || c % 5 == 0)
+{
+sum = c + sum;
+}
+}
+
+printf("%d\n", sum);
+return (0);
 }
