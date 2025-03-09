3a25c5eb0d3d0272814ee3b6260ac0d429397dc8
Monica-Holberton <monicafarid7@gmail.com>
2025-03-09 19:44:48 +0000

    Comb 3!

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -7,19 +7,19 @@
  */
 int main(void)
 {
-    int i, j;
+int i, j;
 
-    for (i = 0; i <= 8; i++) /* First digit (0-8) */
-    {
-        for (j = i + 1; j <= 9; j++) /* Second digit (1-9), always > i */
-        {
-            putchar(i + '0'); /* Convert int to char */
-            putchar(j + '0'); /* Convert int to char */
+for (i = 0; i <= 8; i++)
+{
+for (j = i + 1; j <= 9; j++)
+{
+putchar(i + '0');
+putchar(j + '0');
 
-            if (i != 8 || j != 9) /* Avoid trailing ", " at the end */
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
+if (i != 8 || j != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
