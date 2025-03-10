97293189d90165b438709977d11238d59bc3e068
Monica-Holberton <monicafarid7@gmail.com>
2025-03-10 14:16:39 +0000

    Main.c!

--- more_functions_nested_loops/main.c
+++ more_functions_nested_loops/main.c
@@ -6,8 +6,8 @@ int main(void)
     char ch1 = 'A';
     char ch2 = 'm';
     
-    printf("%c: %d\n", ch1, _isupper(ch1)); // Expected output: A: 1
-    printf("%c: %d\n", ch2, _isupper(ch2)); // Expected output: m: 0
+    printf("%c: %d\n", ch1, _isupper(ch1)); / Expected output: A: 1
+    printf("%c: %d\n", ch2, _isupper(ch2)); / Expected output: m: 0
     
     return (0);
 }
