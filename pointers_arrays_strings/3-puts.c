858abe3c672f82ad79759047e372d90266fac604
Monica-Holberton <monicafarid7@gmail.com>
2025-03-11 18:11:07 +0000

    Puts!

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _puts - Prints a string.
@@ -14,10 +13,4 @@ str++;
 }
 
 _putchar('\n');
-}
-int main(void)
-{
-_puts("Holberton!");
-_puts("");
-return (0);
 }
