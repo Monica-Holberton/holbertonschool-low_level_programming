cdf264bb1750e4f7e0013a49a72b86e13987562c
Monica-Holberton <monicafarid7@gmail.com>
2025-03-16 20:47:07 +0000

    Main.c!

--- more_functions_nested_loops/main.c
+++ more_functions_nested_loops/main.c
@@ -1,32 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 
-/**
- * main - Check the _isupper function.
- *
- * Return: Always 0.
- */
 int main(void)
 {
-char c;
-
-c = 'A';
-printf("%c: %d\n", c, _isupper(c));
-
-c = 'a';
-printf("%c: %d\n", c, _isupper(c));
-
-c = 'Z';
-printf("%c: %d\n", c, _isupper(c));
-
-c = 'z';
-printf("%c: %d\n", c, _isupper(c));
-
-c = '0';
-printf("%c: %d\n", c, _isupper(c));
-
-c = '@';
-printf("%c: %d\n", c, _isupper(c));
-
-return (0);
+    int result = _isdigit('5');
+    printf("%d\n", result);
+    return (0);
 }
