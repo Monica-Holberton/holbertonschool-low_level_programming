d35b0dd6044d534a97a7c7734eb17055f372fbd1
Monica-Holberton <monicafarid7@gmail.com>
2025-03-16 21:20:25 +0000

    Multiply

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,14 +1,29 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * mul - Multiplies two integers
- * _isupper - Checks if a character is an uppercase letter
- * @a: The first integer
- * @b: The second integer
+ * @a: First integer
+ * @b: Second integer
  *
- * Return: The result of multiplying a and b
+ * Return: The product of a and b
  */
 int mul(int a, int b)
 {
 return (a * b);
+}
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Calls the `mul` function to multiply two integers
+ * and prints the result.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int result = mul(3, 4);
+printf("%d\n", result);
+return (0);
 }
