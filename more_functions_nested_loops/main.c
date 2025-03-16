40fe2820ffa6588a2b5566d8762a38f1cbba6232
Monica-Holberton <monicafarid7@gmail.com>
2025-03-16 21:12:37 +0000

    Fix undefined reference error

--- more_functions_nested_loops/main.c
+++ more_functions_nested_loops/main.c
@@ -1,6 +1,13 @@
 #include "main.h"
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
+int result = _isdigit('5');
+printf("%d\n", result);
 return (0);
 }
