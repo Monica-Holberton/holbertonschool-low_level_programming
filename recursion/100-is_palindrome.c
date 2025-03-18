492da71f6f8e051ea9de9569db1bd136d7cf1b63
Monica-Holberton <monicafarid7@gmail.com>
2025-03-18 20:06:04 +0000

    Palindrome!

---
+++ recursion/100-is_palindrome.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to measure.
+ *
+ * Return: The length of the string.
+ */
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+return (0);
+return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * check_palindrome - check if a string is a palindrome.
+ * @s: The string to check.
+ * @left: The starting index.
+ * @right: The ending index.
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise.
+ */
+
+int check_palindrome(char *s, int left, int right)
+{
+if (left >= right)
+return (1);
+
+if (s[left] != s[right])
+return (0);
+
+return (check_palindrome(s, left + 1, right - 1));
+}
+
+/**
+ * is_palindrome - check if a string is a palindrome.
+ * @s: The string to check.
+ *
+ * Return: 1 if `s` is a palindrome, 0 otherwise.
+ */
+
+int is_palindrome(char *s)
+{
+if (*s == '\0')
+return (1);
+
+int length = _strlen_recursion(s);
+return (check_palindrome(s, 0, length - 1));
+}
--- recursion/main.h
+++ recursion/main.h
@@ -18,5 +18,6 @@ int _isupper(int c);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int factorial(int n);
+int is_palindrome(char *s);
 
 #endif /* MAIN_H */
