aee48975e4cfccfe7713e9a6729b6feb945e2df1
Monica-Holberton <monicafarid7@gmail.com>
2025-03-18 20:34:26 +0000

    main.c!

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -14,19 +14,17 @@ return (1 + _strlen_recursion(s + 1));
 }
 
 /**
- * check_palindrome - check if a string is a palindrome.
+ * check_palindrome - Recursively checks if a string is a palindrome.
  * @s: The string to check.
- * @left: The starting index.
- * @right: The ending index.
+ * @left: The left index.
+ * @right: The right index.
  *
  * Return: 1 if the string is a palindrome, 0 otherwise.
  */
-
 int check_palindrome(char *s, int left, int right)
 {
 if (left >= right)
 return (1);
-
 if (s[left] != s[right])
 return (0);
 
@@ -34,17 +32,15 @@ return (check_palindrome(s, left + 1, right - 1));
 }
 
 /**
- * is_palindrome - check if a string is a palindrome.
+ * is_palindrome - Determines if a string is a palindrome.
  * @s: The string to check.
  *
  * Return: 1 if `s` is a palindrome, 0 otherwise.
  */
-
 int is_palindrome(char *s)
 {
 if (*s == '\0')
 return (1);
 
-int length = _strlen_recursion(s);
-return (check_palindrome(s, 0, length - 1));
+return (check_palindrome(s, 0, _strlen_recursion(s) - 1));
 }
--- recursion/main.c
+++ recursion/main.c
@@ -1,22 +1,17 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-    int r;
-
-    r = is_palindrome("level");
-    printf("%d\n", r);
-    r = is_palindrome("redder");
-    printf("%d\n", r);
-    r = is_palindrome("test");
-    printf("%d\n", r);
-    r = is_palindrome("step on no pets");
-    printf("%d\n", r);
-    return (0);
+printf("%d\n", is_palindrome("madam"));
+printf("%d\n", is_palindrome("hello"));
+printf("%d\n", is_palindrome("racecar"));
+printf("%d\n", is_palindrome(""));
+return (0);
 }
