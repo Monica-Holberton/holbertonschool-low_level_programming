9ef7ce3544fb6257070636ee4de1d4c72eab91a0
Monica-Holberton <monicafarid7@gmail.com>
2025-03-19 20:06:52 +0000

    Add!

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,18 +1,20 @@
+/*
+* Program: Add Positive Numbers
+* Description: Adds positive numbers as command-line and print the sum.
+* Prints "0" if no numbers are provided.
+* Prints "Error" and returns 1 if any argument is not a valid number.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <ctype.h>
 
- /*
-  * is_number : a function Checks if a string contains only digit character.
-  * Program: Add Positive Numbers
-  * Description: Adds positive numbers as command-line args and prints sum.
-  * Prints "0" if no numbers are provided.
-  * Prints "Error" and returns 1 if any argument is not a valid number.
-  * Checks if a string contains only digits.
-  * str - The string to check.
-  * Return: 1 - If the string contains only digits, 0 - Otherwise.
-  */
+/**
+* is_number - Checks if a string contains only digit characters.
+* @str: The string to check.
+*
+* Return: 1 if the string contains only digits, 0 otherwise.
+*/
 int is_number(char *str)
 {
 for (int i = 0; str[i] != '\0'; i++)
@@ -23,25 +25,21 @@ return (0);
 return (1);
 }
 
- /*
-  * main - Adds positive numbers from command-line arguments.
-  *
-  * Parameters:
-  * @argc - Argument count.
-  * @argv - Argument vector.
-  *
-  * Return: Always (0)
-  */
+/**
+* main - Adds positive numbers from command-line arguments.
+* @argc: Argument count.
+* @argv: Argument values.
+*
+* Return: 0 on success, 1 on error.
+*/
 int main(int argc, char *argv[])
 {
 int sum = 0;
-
 if (argc == 1)
 {
 printf("0\n");
 return (0);
 }
-
 for (int i = 1; i < argc; i++)
 {
 if (!is_number(argv[i]))
@@ -51,7 +49,6 @@ return (1);
 }
 sum += atoi(argv[i]);
 }
-
 printf("%d\n", sum);
 return (0);
 }
--- argc_argv/main.c
+++ argc_argv/main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
  /*
 * main: prints the name of the program, including its full path.
