8442fcd4bec40838a87944cb3cb642bb551134de
Monica-Holberton <monicafarid7@gmail.com>
2025-03-20 14:03:50 +0000

    Args!

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
-
- /*
-  * main: prints the number of arguments passed to the program.
-  *
-  * Parameters:
-  * @argc: argument count
-  * @argv: argument vector
-  * Program: Print Argument Count
-  * Description: print the number of arguments passed into it,
-  * followed by a new line.
-  * Return: Always (0)
-  */
+#define UNUSED(x) (void)(x)
+/**
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ *Return: always 0
+ */
 int main(int argc, char *argv[])
 {
-(void)argv;
+UNUSED(argv);
 printf("%d\n", argc - 1);
 return (0);
 }
