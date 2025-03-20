33797d28ec986191d9a16d698af9a515d39bf189
Monica-Holberton <monicafarid7@gmail.com>
2025-03-20 19:41:45 +0000

    Multiple!

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -6,25 +6,23 @@
  */
 void print_triangle(int size)
 {
-    int i, j, spaces;
+int i, j, spaces;
 
-    if (size > 0)
-    {
-        for (i = 1; i <= size; i++)
-        {
-            /* Print spaces */
-            for (spaces = size - i; spaces > 0; spaces--)
-                _putchar(' ');
+if (size > 0)
+{
+for (i = 1; i <= size; i++)
+{
+for (spaces = size - i; spaces > 0; spaces--)
+_putchar(' ');
 
-            /* Print '#' */
-            for (j = 0; j < i; j++)
-                _putchar('#');
+for (j = 0; j < i; j++)
+_putchar('#');
 
-            _putchar('\n'); /* Move to the next line */
-        }
-    }
-    else
-    {
-        _putchar('\n'); /* Print newline if size <= 0 */
-    }
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
 }
