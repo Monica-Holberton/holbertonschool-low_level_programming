d8f2c4033889eaf32ac043553957e492c5e3f4bd
Monica-Holberton <monicafarid7@gmail.com>
2025-03-20 14:09:09 +0000

    Multiply!

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,26 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
- /*
- * main - start of the program
- * Description: multiplies two numbers as command-line arguments
- * and prints the result, followed by a new line.
- * Parameters:
- * @argc: argument count
- * @argv: argument vector
- * Return:Always (0)
+/**
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ *Return: always 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char  *argv[])
 {
-if (argc != 3)
+if (argc == 3)
 {
-printf("Error\n");
-return 1;
-}
-
-int no1 = atoi(argv[1]);
-int no2 = atoi(argv[2]);
-printf("%d\n", no1 * no2);
- 
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 return (0);
+}
+else
+printf("Error\n");
+return (1);
 }
