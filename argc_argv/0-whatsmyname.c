b046ea4d94392d9118e3f27dafd15daf84d853bd
Monica-Holberton <monicafarid7@gmail.com>
2025-03-20 14:01:21 +0000

    What's My Name?!

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
-#include <main.h>
-
- /*
-  * main: start the program, including its full path.
-  * If the program is renamed, the new name will be printed automatically.
- * Program: Print Program Name
- * Description: This program prints its own name, followed by a new line.
- * If the program is renamed, it will print the new name without recompilation.
- * The full path is included in the output.
-  * Parameters:
-* @argc: argument count
-* @argv: argument vector
-  * Return: Always (0)
-  */
+#define UNUSED(x) (void)(x)
+/**
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ *Return: always 0
+ */
 int main(int argc, char *argv[])
 {
-(void)argc;
+UNUSED(argc);
 printf("%s\n", argv[0]);
 return (0);
 }
