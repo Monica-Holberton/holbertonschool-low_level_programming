145d9b3e45580e8d6ad88622e16708528988b995
Monica-Holberton <monicafarid7@gmail.com>
2025-03-20 14:05:38 +0000

    Args.2!

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-
- /*
- * main - print all arguments received, one per line.
- *
- * Parameters:
- * @argc: argument count
- * @argv: argument vector
- * Program: Print All Arguments
- * Description: prints all arguments it receives, including the file name
- * Return: Always (0)
+#define UNUSED(x) (void)(x)
+/**
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ *Return: always 0
  */
 int main(int argc, char *argv[])
 {
