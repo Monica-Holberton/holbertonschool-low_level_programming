cbaa1695d829ade7108fa1b0c2e52fb0874bc337
Monica-Holberton <monicafarid7@gmail.com>
2025-03-20 14:29:48 +0000

    Add!

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -2,56 +2,47 @@
 #include <stdlib.h>
 #include <ctype.h>
 #define UNUSED(x) (void)(x)
-/**
- * StringCheck - checks string
- * @s: string to check
- * Return: always 0
- */
-int StringCheck(char *s)
-{
-int i = 0;
 
-for (; s[i] != '\0'; i++)
+/**
+* is_number - Checks if a string contains only digit characters.
+* @str: The string to check.
+ * Return: boolean
+ */
+
+int is_number(char *str)
 {
-if (!isdigit(s[i]))
+for (int i = 0; str[i] != '\0'; i++)
 {
+if (!isdigit(str[i]))
 return (0);
-}
 }
 return (1);
 }
+
 /**
  * main - main function
  * @argc: argumentc
  * @argv: vector of arguments
  *Return: always 0
  */
-int main(int argc, char  *argv[])
-{
-int i;
-int result = 0;
 
-if (argc > 1)
+int main(int argc, char *argv[])
 {
-for (i = 1; i < argc; i++)
+int sum = 0;
+if (argc == 1)
 {
-if (StringCheck(argv[i]))
-{
-result += atoi(argv[i]);
+printf("0\n");
+return (0);
 }
-else
+for (int i = 1; i < argc; i++)
+{
+if (!is_number(argv[i]))
 {
 printf("Error\n");
 return (1);
 }
+sum += atoi(argv[i]);
 }
-printf("%d\n", result);
+printf("%d\n", sum);
 return (0);
-}
-else
-{
-printf("%d\n", 0);
-return (1);
-}
-
 }
