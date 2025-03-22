7058a0d652036131b6ab748b9cb9edcf10afb4a1
Monica-Holberton <monicafarid7@gmail.com>
2025-03-22 20:59:01 +0000

    Magic!

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+
 /**
- * main - Entry point of the program
+ * main - Demonstrates pointer manipulation to modify an array
  *
- * Description: This program demonstrates pointer manipulation
- * to modify an array value without directly using the array variable.
+ * Description:
+ * This program initializes an array and a pointer, then uses pointer
+ * arithmetic to modify the value of a specific element in the array
+ * without directly referencing it. The goal is to change `a[2]` to 98
+ * by using only pointer operations.
  *
  * Return: Always 0 (Success)
-*/
+ */
 int main(void)
 {
 int n;
@@ -16,21 +20,18 @@ int *p;
 a[2] = 1024;
 p = &n;
 
-/*
-* - you are not allowed to use the variable a
-* - you are not allowed to modify the variable p
-* - you can only write one statement
-* - you are not allowed to use ,
-* - you are not allowed to code anything else than the expected line of code
-* - do not remove anything from the initial code (not even the comments)
-* - don’t change the spaces to tabs!
-* - you are allowed to use the standard library
-*/
+    /*
+     * write your line of code here...
+     * Remember:
+     * - you are not allowed to use the variable a
+     * - you are not allowed to modify the variable p
+     * - you can only write one statement
+     * - you are not allowed to use ,
+     * - you are not allowed to code anything else than the line of code
+     * - you are allowed to use the standard library
+     */
 *(p + 5) = 98;
-
-printf("a[2] = ");
-printf("%d", a[2]);
-printf("\n");
+printf("a[2] = %d\n", a[2]);
 
 return (0);
 }
