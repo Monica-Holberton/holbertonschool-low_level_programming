3432b7b8a65cb1077b0e3dbb582b76a67736f6a6
Monica-Holberton <monicafarid7@gmail.com>
2025-03-22 21:25:37 +0000

    Magic!

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Demonstrates pointer manipulation to modify an array
  *
@@ -7,25 +6,15 @@
  */
 int main(void)
 {
+
 int n;
 int a[5];
 int *p;
-
 a[2] = 1024;
 p = &n;
 
-    /*
-     * write your line of code here...
-     * Remember:
-     * - you are not allowed to use the variable a
-     * - you are not allowed to modify the variable p
-     * - you can only write one statement
-     * - you are not allowed to use ,
-     * - you are not allowed to code anything else than the line of code
-     * - you are allowed to use the standard library
-     */
 *(p + 5) = 98;
-    /* ...so that this prints "a[2] = 98\n" */
 puts("a[2] = 98");
+
 return (0);
 }
