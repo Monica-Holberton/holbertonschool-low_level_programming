0882b785904b9dc60f252c4509474eb547e06dc0
Monica-Holberton <monicafarid7@gmail.com>
2025-03-23 14:55:02 +0000

    Main.h!

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - Frees a 2D grid previously allocated by alloc_grid.
+ * @grid: The 2D array to be freed.
+ * @height: The number of rows in the grid.
+ */
+void free_grid(int **grid, int height)
+{
+int i;
+
+if (grid == NULL || height <= 0)
+return;
+
+for (i = 0; i < height; i++)
+free(grid[i]);
+    
+free(grid);
+}
