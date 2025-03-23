9626137bc6df900d2aa8e36491d74d8a6f5756f6
Monica-Holberton <monicafarid7@gmail.com>
2025-03-23 14:32:43 +0000

    Double String!

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -13,18 +13,18 @@ char *dup;
 unsigned int i, len = 0;
 
 if (str == NULL)
-return NULL;
+return (NULL);
 
 while (str[len])
 len++;
 
 dup = malloc(sizeof(char) * (len + 1));
 if (dup == NULL)
-return NULL;
+return (NULL);
 
 for (i = 0; i < len; i++)
 dup[i] = str[i];
 dup[len] = '\0';
 
-return dup;
+return (dup);
 }
