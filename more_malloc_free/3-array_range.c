975c5b0595025ca2d2cdd744ce75d092bd554688
Monica-Holberton <monicafarid7@gmail.com>
2025-03-26 20:15:22 +0000

    Array Range!

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -9,19 +9,19 @@
  */
 int *array_range(int min, int max)
 {
-    int *arr;
-    int i, size;
+int *arr;
+int i, size;
 
     /* Return NULL if min is greater than max */
-    if (min > max)
-        return (NULL);
+if (min > max)
+return (NULL);
 
     /* Calculate the size of the array */
-    size = max - min + 1;
+size = max - min + 1;
 
     /* Allocate memory for the array */
-    arr = malloc(sizeof(int) * size);
-    if (arr == NULL)
+arr = malloc(sizeof(int) * size);
+if (arr == NULL)
 return (NULL);
 
     /* Fill the array with values from min to max */
