9e48fd86bbc9d5dde7a4f2edc6593e177d5ea0b9
Monica-Holberton <monicafarid7@gmail.com>
2025-04-02 15:03:32 +0000

    Print Dog!

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,8 +12,8 @@
 void print_dog(struct dog *d)
 {
 if (d)
-printf("Name: %s\nAge: %.1f\nOwner: %s\n",
-d->name ? d->name : "(nil)",
-d->age,
-d->owner ? d->owner : "(nil)");
+printf("Name: %s\n", d->name ? d->name : "(nil)");
+printf("Age: %.1f\n", d->age);
+printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
+
