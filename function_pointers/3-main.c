2c765586fa51035c2baf69a918ad80c35d240988
Monica-Holberton <monicafarid7@gmail.com>
2025-04-04 00:16:10 +0000

    Multiple Operations!

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,34 +1,46 @@
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - performs simple operations
- * @argc: number of arguments
- * @argv: arguments
- * Return: 0 on success, exit with status on error
+ * main - Entry point. Performs simple arithmetic operations.
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ * Return: 0 on success, exits with specific error codes otherwise
  */
 int main(int argc, char *argv[])
 {
-int num1, num2, result;
-int (*op_func)(int, int);
+    int num1, num2, result;
+    int (*operation)(int, int);
 
-if (argc != 4)
-{
-printf("Error\n");
-exit(98);
-}
+    /* Check if the number of arguments is correct */
+    if (argc != 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
 
-num1 = atoi(argv[1]);
-num2 = atoi(argv[3]);
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[3]);
 
-op_func = get_op_func(argv[2]);
-if (op_func == NULL)
-{
-printf("Error\n");
-exit(99);
-}
+    /* Get the operation function */
+    operation = get_op_func(argv[2]);
+    if (operation == NULL)
+    {
+        printf("Error\n");
+        exit(99);
+    }
 
-result = op_func(num1, num2);
-printf("%d\n", result);
+    /* Check for division or modulo by zero */
+    if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+    {
+        printf("Error\n");
+        exit(100);
+    }
 
-return (0);
+    /* Perform the operation */
+    result = operation(num1, num2);
+    printf("%d\n", result);
+
+    return (0);
 }
