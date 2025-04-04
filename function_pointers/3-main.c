0b4f55061553cf16dadd88267e3b6b3f05ceea3f
Monica-Holberton <monicafarid7@gmail.com>
2025-04-04 00:18:31 +0000

    Multiple Operations!

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -10,37 +10,37 @@
  */
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
-    int (*operation)(int, int);
+int num1, num2, result;
+int (*operation)(int, int);
 
-    /* Check if the number of arguments is correct */
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+/* Check if the number of arguments is correct */
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
 
-    /* Get the operation function */
-    operation = get_op_func(argv[2]);
-    if (operation == NULL)
-    {
-        printf("Error\n");
-        exit(99);
-    }
+/* Get the operation function */
+operation = get_op_func(argv[2]);
+if (operation == NULL)
+{
+printf("Error\n");
+exit(99);
+}
 
-    /* Check for division or modulo by zero */
-    if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
+/* Check for division or modulo by zero */
+if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+{
+printf("Error\n");
+exit(100);
+}
 
-    /* Perform the operation */
-    result = operation(num1, num2);
-    printf("%d\n", result);
+/* Perform the operation */
+result = operation(num1, num2);
+printf("%d\n", result);
 
-    return (0);
+return (0);
 }
