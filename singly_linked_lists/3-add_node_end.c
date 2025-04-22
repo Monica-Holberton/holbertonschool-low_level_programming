6e03f89eaa241f9a6e8145989b5d35c80d2a3a87
Monica-Holberton <monicafarid7@gmail.com>
2025-04-22 20:30:53 +0000

    Add Node End!

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -3,51 +3,59 @@
 #include "lists.h"
 
 /**
- * add_node_end - adds a new node at the end of a list_t list
- * @head: pointer to the pointer of the head node
- * @str: string to be duplicated and added to the new node
- *
- * Return: address of the new element, or NULL if it failed
+* add_node_end - adds a new node at the end of a list_t list
+* @head: double pointer to the head of the list
+* @str: string to duplicate and add in the new node
+*
+* Return: address of the new element, or NULL if it failed
 */
 list_t *add_node_end(list_t **head, const char *str)
 {
-list_t *new_node;
-list_t *last_node;
+list_t *new_node, *temp;
+char *dup_str;
+unsigned int len = 0;
 
-if (str == NULL)
-return (NULL);
+/* Duplicate the string */
+if (str != NULL)
+{
+dup_str = strdup(str);
+if (dup_str == NULL)
+return (NULL); /* strdup failed */
+while (str[len])
+len++;
+}
+else
+{
+dup_str = NULL;
+len = 0;
+}
 
 /* Allocate memory for the new node */
 new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
-return (NULL);
-
-/* Duplicate the input string */
-new_node->str = strdup(str);
-if (new_node->str == NULL)
 {
-free(new_node);
+free(dup_str); /* Free the duplicated string if malloc fails */
 return (NULL);
 }
 
-/* Set fields of new node */
-new_node->len = strlen(str);
+/* Initialize the new node */
+new_node->str = dup_str;
+new_node->len = len;
 new_node->next = NULL;
 
-/* If the list is empty, set new node as the head */
+/* If the list is empty, set head to new node */
 if (*head == NULL)
 {
 *head = new_node;
-return (new_node);
 }
-
-/* Check the end of the list */
-last_node = *head;
-while (last_node->next != NULL)
-last_node = last_node->next;
-
-/* Add new node at the end */
-last_node->next = new_node;
+else
+{
+/* Otherwise, traverse to the end and add node */
+temp = *head;
+while (temp->next != NULL)
+temp = temp->next;
+temp->next = new_node;
+}
 
 return (new_node);
 }
