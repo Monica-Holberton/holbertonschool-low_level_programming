622c4c24f6e52114ad3261634265a550bcd5e9e9
Monica-Holberton <monicafarid7@gmail.com>
2025-04-23 19:08:10 +0000

    Print dlistint_t!

--- doubly_linked_lists/main.c
+++ doubly_linked_lists/main.c
@@ -10,16 +10,27 @@
  */
 int main(void)
 {
-dlistint_t *head;
 dlistint_t *new;
-dlistint_t hello = {8, NULL, NULL};
-size_t n;
+dlistint_t hello = {8, NULL, NULL};  /* Initial node (head) */
 
-head = &hello;
+/* Allocate memory for the new node */
 new = malloc(sizeof(dlistint_t));
 if (new == NULL)
 {
 dprintf(2, "Error: Can't malloc\n");
 return (EXIT_FAILURE);
 }
+
+new->n = 9;  /* Assign a value to the new node */
+new->prev = NULL;  /* The new node will be the new head, so it has no previous node */
+new->next = &hello;  /* The new node’s next will point to the current hello node */
+hello.prev = new;  /* Update the current hello node to point back to the new node */
+
+/* Print the list and get the number of nodes */
+print_dlistint(new);
+printf("-> 2 elements\n");
+
+free(new);
+
+return (EXIT_SUCCESS);
 }
