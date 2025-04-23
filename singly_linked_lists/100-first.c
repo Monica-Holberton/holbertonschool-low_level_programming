958ccfc4fb70dbbe76048edc2ebeae142c55b215
Monica-Holberton <monicafarid7@gmail.com>
2025-04-23 14:42:57 +0000

    First Tortoise!

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * first - prints a message before the main function is executed
+ */
+
+void print_before_main(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
