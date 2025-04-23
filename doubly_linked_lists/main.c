16b7e039497b54cd51eb00cb043a3e5a44d59921
Monica-Holberton <monicafarid7@gmail.com>
2025-04-23 19:24:13 +0000

    dlistint_t Length!

--- doubly_linked_lists/main.c
+++ doubly_linked_lists/main.c
@@ -4,33 +4,31 @@
 #include "lists.h"
 
 /**
- * main - check the code
- *
- * Return: Always EXIT_SUCCESS.
- */
+* main - check the code
+*
+* Return: Always EXIT_SUCCESS.
+*/
 int main(void)
 {
+dlistint_t *head;
 dlistint_t *new;
-dlistint_t hello = {8, NULL, NULL};  /* Initial node (head) */
+dlistint_t hello = {8, NULL, NULL};
+size_t n;
 
-/* Allocate memory for the new node */
+head = &hello;
 new = malloc(sizeof(dlistint_t));
 if (new == NULL)
 {
 dprintf(2, "Error: Can't malloc\n");
 return (EXIT_FAILURE);
 }
-
-new->n = 9;  /* Assign a value to the new node */
-new->prev = NULL;  /* The new node will be the new head, so it has no previous node */
-new->next = &hello;  /* The new node’s next will point to the current hello node */
-hello.prev = new;  /* Update the current hello node to point back to the new node */
-
-/* Print the list and get the number of nodes */
-print_dlistint(new);
-printf("-> 2 elements\n");
-
+new->n = 9;
+head->prev = new;
+new->next = head;
+new->prev = NULL;
+head = new;
+n = dlistint_len(head);
+printf("-> %lu elements\n", n);
 free(new);
-
 return (EXIT_SUCCESS);
 }
--- doubly_linked_lists/main.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stddef.h>
-
-size_t print_dlistint(const dlistint_t *h);
-
-#endif /* MAIN_H */
